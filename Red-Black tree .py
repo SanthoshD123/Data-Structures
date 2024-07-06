0f4087a4113e8c9f891d29016b84653f09893727
Santhosh D <141542516+SanthoshD123@users.noreply.github.com>
2024-07-06 21:19:14 +0530

    Create Red-Black tree .py

---
+++ Red-Black tree .py
@@ -0,0 +1,24 @@
+class RBNode:
+    def __init__(self, value, color='red'):
+        self.value = value
+        self.color = color
+        self.left = None
+        self.right = None
+        self.parent = None
+
+    def grandparent(self):
+        if self.parent is None:
+            return None
+        return self.parent.parent
+
+    def sibling(self):
+        if self.parent is None:
+            return None
+        if self == self.parent.left:
+            return self.parent.right
+        return self.parent.left
+
+    def uncle(self):
+        if self.parent is None:
+            return None
+        return self.parent.sibling()
