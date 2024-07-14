87077aa38f61a3328ba589875959bca5211380b0
Santhosh D <141542516+SanthoshD123@users.noreply.github.com>
2024-07-14 20:31:46 +0530

    Create Heapify implementation .py

---
+++ Heapify implementation .py
@@ -0,0 +1,34 @@
+def heapify(arr, n, i):
+    largest = i  # Initialize largest as root
+    left = 2 * i + 1  # left child
+    right = 2 * i + 2  # right child
+
+    # See if left child of root exists and is greater than root
+    if left < n and arr[i] < arr[left]:
+        largest = left
+
+    # See if right child of root exists and is greater than the largest so far
+    if right < n and arr[largest] < arr[right]:
+        largest = right
+
+    # Change root, if needed
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]  # swap
+
+        # Heapify the root.
+        heapify(arr, n, largest)
+
+def build_heap(arr):
+    n = len(arr)
+
+    # Build a maxheap.
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    return arr
+
+if __name__ == "__main__":
+    my_list = [3, 2, 5, 1, 7, 8]
+    print("Original list:", my_list)
+    heapified_list = build_heap(my_list)
+    print("Heapified list:", heapified_list)
